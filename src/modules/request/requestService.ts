47a37ff82b9b5321dbd3bc29d27a84d6c0982fd1
Kalidash Odekare <shaktiadhikari49@gmail.com>
2026-05-27 14:57:40 +0600

    provider offer create api implemented

--- src/modules/request/requestService.ts
+++ src/modules/request/requestService.ts
@@ -68,7 +68,6 @@ const deleteRequest = async (userId: string) => {
 };
 
 const getOffersForRequest = async (requestId: string) => {
-  console.log('Fetching offers for request ID:', requestId);
   const offers = await Offer.find({
     request: requestId,
     status: { $in: ['offered', 'accepted'] },
